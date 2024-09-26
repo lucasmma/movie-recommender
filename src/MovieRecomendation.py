0837fca9fc1544354836a7ad1b6509b359736a95
Lucas Mendonça <lukito020@gmail.com>
2024-09-26 18:12:27 -0300

    feat: add correct calc for probabilities

--- src/MovieRecomendation.py
+++ src/MovieRecomendation.py
@@ -20,12 +20,24 @@ class MovieRecomendation:
                                                             n_neighbors=size + 1)
                 rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(), distances.squeeze().tolist())),
                                         key=lambda x: x[1])[:0:-1]
+                # Convert distances to similarity probabilities
+                # Inverse distance method: 1/distance
+                inverse_distances = [1 / val[1] for val in rec_movie_indices]
+                # Normalize the probabilities
+                total_inverse_distance = sum(inverse_distances)
+                # calculate the probabilities
+                probabilities = [inv_dist / total_inverse_distance for inv_dist in inverse_distances]
+                
                 recommend_frame = []
-                for val in rec_movie_indices:
+                for idx, val in enumerate(rec_movie_indices):
                     movie_idx = self.final_dataset.iloc[val[0]]['movieId']
-                    idx = self.movies[self.movies['movieId'] == movie_idx].index
-                    recommend_frame.append({'Title': self.movies.iloc[idx]['title'].values[0], 'Distance': val[1], 'Id': movie_idx})
-                df = pd.DataFrame(recommend_frame, index=range(1, size + 1))
+                    movie_title_idx = self.movies[self.movies['movieId'] == movie_idx].index
+                    recommend_frame.append({
+                        'Title': self.movies.iloc[movie_title_idx]['title'].values[0],
+                        'Probability': probabilities[idx],
+                        'Id': movie_idx
+                    })
+                df = pd.DataFrame(recommend_frame[::-1], index=range(1, size + 1))
                 return df
             else:
                 return "No movies found. Please check your input"
